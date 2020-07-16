3ae66f037131ce994d903b831d0dcc779b468b0d
Vitor Santana Cordeiro <vitorsanusp@gmail.com>
2020-07-16 18:18:37 -0300

    populating
    Co-authored-by: JVSRamos <joaoramosj@usp.br>
    Co-authored-by: MarceloMoraesJr <marcelo.junior@usp.br>
    Co-authored-by: giovanidl <giovanilucafo@usp.br>

--- 2-data-setup.sql
+++ 2-data-setup.sql
@@ -7,9 +7,9 @@ INSERT INTO CLIENTE (CPF, EMAIL, DATA_INSCRICAO, NOME, TELEFONE, DATA_NASCIMENTO
 -- Vitor
 INSERT INTO CLIENTE (CPF, EMAIL, DATA_INSCRICAO, NOME, TELEFONE, DATA_NASCIMENTO, SENHA, FOTO, AVALIACAO_GERAL)
     VALUES ('49061357810', 'vitorsan@bd.com', '2018-08-21 22:08:25-03', 'Vitor Santana Cordeiro', '11947836937', '9/20/2000', 'vamopobackvitao', './photos/vitor.jpeg', 4.5);
--- Marcelo Agiota
+-- Marcelo 
 INSERT INTO CLIENTE (CPF, EMAIL, DATA_INSCRICAO, NOME, TELEFONE, DATA_NASCIMENTO, SENHA, FOTO, AVALIACAO_GERAL)
-    VALUES ('47495856882', 'marcelo_agiota@hotmail.com', '2016-09-22 22:05:12-03', 'Marcelo Agiotao', '17996698335', '7/15/1999', 'fullstackagiotao','./photos/marcelo_agiotao.jpeg', 5.00);
+    VALUES ('47495856882', 'marcelo@hotmail.com', '2016-09-22 22:05:12-03', 'Marcelo Moraes', '17996698335', '7/15/1999', 'fullstacko','./photos/marcelo.jpeg', 5.00);
 -- Giovani
 INSERT INTO CLIENTE (CPF, EMAIL, DATA_INSCRICAO, NOME, TELEFONE, DATA_NASCIMENTO, SENHA, FOTO, AVALIACAO_GERAL)
     VALUES ('46975897859', 'giovanilucafo@gmail.com', '2015-07-21 16:07:32-03', 'Giovani Decico Lucafo','19981216021','9/26/1900', 'coxinha26091997','./photos/giovani.jpeg', 4.20);
@@ -18,30 +18,31 @@ INSERT INTO CLIENTE (CPF, EMAIL, DATA_INSCRICAO, NOME, TELEFONE, DATA_NASCIMENTO
     VALUES ('11111111111', 'craqueneto10@gmail.com', '2014-12-10 21:13:33-03', 'José Ferreira Neto','11999999999','9/9/1966', 'cabelodeboneca', './photos/craque_neto.jpeg', 2.0);
 
 -- NEGOCIANTE
--- Joao
+-- Joao (Locador, Vendedor)
 INSERT INTO NEGOCIANTE (CPF, EMAIL, DATA_INSCRICAO, NOME, TELEFONE, DATA_NASCIMENTO, SENHA, FOTO, AVALIACAO_GERAL, EH_PRESTADOR, EH_LOCADOR, EH_VENDEDOR)
-    VALUES ('48487098819', 'joaoramosj@outlook.com', '2016-10-20 22:08:24-05', 'Joao Vitor Silva Ramos', '17996698335', '8/6/2000', 'senha123', './photos/joao.jpeg', NULL, FALSE, FALSE, TRUE);
--- Craque neto
+    VALUES ('48487098819', 'joaoramosj@outlook.com', '2016-10-20 22:08:24-05', 'Joao Vitor Silva Ramos', '17996698335', '8/6/2000', 'senha123', './photos/joao.jpeg', NULL, FALSE, TRUE, TRUE);
+-- Craque neto (Locador, Vendedor)
 INSERT INTO NEGOCIANTE (CPF, EMAIL, DATA_INSCRICAO, NOME, TELEFONE, DATA_NASCIMENTO, SENHA, FOTO, AVALIACAO_GERAL, EH_PRESTADOR, EH_LOCADOR, EH_VENDEDOR)
     VALUES ('11111111111', 'craqueneto10@gmail.com', '2014-12-10 21:50:53-03', 'José Ferreira Neto','11999999999','9/9/1966', 'cabelodeboneca', './photos/craque_neto.jpeg', 4.0, FALSE, TRUE, TRUE);
--- Kasalbe
+-- Kasalbe (Locador)
 INSERT INTO NEGOCIANTE (CPF, EMAIL, DATA_INSCRICAO, NOME, TELEFONE, DATA_NASCIMENTO, SENHA, FOTO, AVALIACAO_GERAL, EH_PRESTADOR, EH_LOCADOR, EH_VENDEDOR)
     VALUES ('44455566677', 'kasalbe@usp.br', '2014-07-12 10:01:46-03', 'Otávio Polozzi Rodrigues', '1691234785', '8/20/1998', 'repsantacasa', './photos/kasalbe.jpeg', 3.8, FALSE, TRUE, FALSE);
--- Marcelo Agiota
+-- Marcelo (Prestador, Locador, Vendedor)
 INSERT INTO NEGOCIANTE (CPF, EMAIL, DATA_INSCRICAO, NOME, TELEFONE, DATA_NASCIMENTO, SENHA, FOTO, AVALIACAO_GERAL, EH_PRESTADOR, EH_LOCADOR, EH_VENDEDOR)
-    VALUES ('47495856882', 'marcelo_agiota@hotmail.com', '2016-09-22 23:05:12-03', 'Marcelo Agiotao', '17996698335', '7/15/1999', 'fullstackagiotao','./photos/marcelo_agiotao.jpeg', 1.00, TRUE, TRUE, TRUE);
--- Carlos Bife
+    VALUES ('47495856882', 'marcelo@hotmail.com', '2016-09-22 23:05:12-03', 'Marcelo Moraes', '17996698335', '7/15/1999', 'fullstacko','./photos/marcelo.jpeg', 1.00, TRUE, TRUE, TRUE);
+-- Carlos Bife (Prestador)
 INSERT INTO NEGOCIANTE (CPF, EMAIL, DATA_INSCRICAO, NOME, TELEFONE, DATA_NASCIMENTO, SENHA, FOTO, AVALIACAO_GERAL, EH_PRESTADOR, EH_LOCADOR, EH_VENDEDOR)
     VALUES ('48121548421', 'carlosbife@hotmail.com', '2010-01-30 23:04:59-03', 'Carlos Bife', '19855515658', '4/22/1982', 'truco','./photos/bife.jpeg', 2.55, TRUE, FALSE, FALSE);
 
 -- AVALIACAO
+-- Cliente (X) Negociante: direcao da avaliacao definida pelo booleano "VEM_DO_CLIENTE" 
 -- Joao -> Carlos Bife
 INSERT INTO AVALIACAO (CLIENTE, NEGOCIANTE, DATA_HORA, TEXTO, RESPOSTA, VEM_DO_CLIENTE, NOTA)
     VALUES ('48487098819', '48121548421', '2019-07-21 16:07:32-03', 'Queria mudar de casa e contratei o Carlos, mas ele demorou muito pra realizar o serviço. Além disso, vi ele jogando truco no posto no horário do serviço','O que vem de baixo nao me atinge seu esquerdista, impedindo que eu me divirta, vai pra cuba se nao quer vida boa', TRUE, 1.00); 
--- Giovani -> Marcelo Agiota
+-- Giovani -> Marcelo 
 INSERT INTO AVALIACAO (CLIENTE, NEGOCIANTE, DATA_HORA, TEXTO, RESPOSTA, VEM_DO_CLIENTE, NOTA)
-    VALUES ('46975897859', '47495856882', '2019-10-21 16:10:42-03', 'Contratei o marcelo para arrumar meu armário, porém descobri depois que ele era agiota e ele veio com uns papo de pagar umas conta pra mim meio estranho, e no fim das contas meu armario fico tudo torto', 'Logico, vc me deve 30k em emprestimos, ja se esqueceu que sua hillux nao veio do suor do seu trabalho?', TRUE, 0.5);
--- Marcelo Agiota -> Joao
+    VALUES ('46975897859', '47495856882', '2019-10-21 16:10:42-03', 'Contratei o marcelo para arrumar meu armário, porém descobri depois que ele era  e ele veio com uns papo de pagar umas conta pra mim meio estranho, e no fim das contas meu armario fico tudo torto', 'Logico, vc me deve 30k em emprestimos, ja se esqueceu que sua hillux nao veio do suor do seu trabalho?', TRUE, 0.5);
+-- Marcelo  -> Joao
 INSERT INTO AVALIACAO (CLIENTE, NEGOCIANTE, DATA_HORA, TEXTO, RESPOSTA, VEM_DO_CLIENTE, NOTA)
     VALUES ('47495856882', '48487098819', '2019-10-09 22:08:24-05', 'Esse joao é muito ruim, queria comprar um liquidificador dele, porém ele mora mais longe que onde o judas perdeu as botas','Calunia! Fernandopolis eh a metropole do futuro', TRUE, 2.40);
 -- Vitor -> Craque Neto   
@@ -53,18 +54,19 @@ INSERT INTO AVALIACAO (CLIENTE, NEGOCIANTE, DATA_HORA, TEXTO, RESPOSTA, VEM_DO_C
 -- Carlos Bife <- Vitor
 INSERT INTO AVALIACAO (CLIENTE, NEGOCIANTE, DATA_HORA, TEXTO, RESPOSTA, VEM_DO_CLIENTE, NOTA)
     VALUES ('48121548421', '49061357810', '2019-10-20 20:08:12-05', 'Eu fui so fazer um trabalhinho na casa do vitor e ele nao parava de falar que ele era um estudante global sla.. acabou que o trabalho fico muito bem feito mas eu tava mesmo era com vtd de xinga ele', NULL, FALSE, 4.0);
--- Vitao-> Kazalbe
+-- Vitor -> Kazalbe
 INSERT INTO AVALIACAO (CLIENTE, NEGOCIANTE, DATA_HORA, TEXTO, RESPOSTA, VEM_DO_CLIENTE, NOTA)
     VALUES ('49061357810', '44455566677', '2019-11-02 20:10:12-03', 'Aluguei uma vaga da Rep. Santa Casa com o Kasalbé e ele foi muito atencioso! Mandou tudo certinho e agilizou bastante o processo! Nota 100 :)', 'Obrigado Vitão!! Um prazer fazer negócios com você.', TRUE, 5.0);
 
 -- DENUNCIA 
--- Marcelo Agiota -> Joao
+-- Cliente (X) Negociante: direcao da avaliacao definida pelo booleano "VEM_DO_CLIENTE" 
+-- Marcelo -> Joao
 INSERT INTO DENUNCIA (CLIENTE, NEGOCIANTE, DATA_HORA, TEXTO, RESPOSTA, VEM_DO_CLIENTE)
     VALUES ('47495856882', '48487098819','2019-08-20 16:07:32-03', 'Não pagou meus produtos, é um pilantra', 'Vai cair dia 15 o salário, eu vou te pagar ;)', FALSE);
 -- Vitor -> Craque Neto
 INSERT INTO DENUNCIA (CLIENTE, NEGOCIANTE, DATA_HORA, TEXTO, RESPOSTA, VEM_DO_CLIENTE)
     VALUES ('49061357810', '11111111111', '2018-10-24 12:15:30-03', 'É um safado. Me vendeu um produto e depois me avaliou mal porque falei umas verdades na cara dele! Um usuário ridículo, deveria ser banido.', NULL, TRUE);
--- Joao -> Marcelo Agiota
+-- Joao -> Marcelo 
 INSERT INTO DENUNCIA (CLIENTE, NEGOCIANTE, DATA_HORA, TEXTO, RESPOSTA, VEM_DO_CLIENTE)
     VALUES ('48487098819', '47495856882', '2018-8-10 8:45:23-03', 'Esse joao me roubou, comprou a minha cadeira de praia favorita mas nao me pagou!', 'Achado nao eh roubado :)', FALSE);
 -- Giovani -> Carlos Bife    
@@ -78,7 +80,7 @@ INSERT INTO CONVERSA (CLIENTE, NEGOCIANTE, DATA_HORA, LOG_CONVERSA)
 -- Vitor e Craque Neto
 INSERT INTO CONVERSA (CLIENTE, NEGOCIANTE, DATA_HORA, LOG_CONVERSA)
     VALUES ('49061357810', '11111111111', '2018-10-28 22:08:10-03', './chat_logs/2018-10-28_22:08:10-03.log');
--- Marcelo Agiota e Carlos Bife
+-- Marcelo e Carlos Bife
 INSERT INTO CONVERSA (CLIENTE, NEGOCIANTE, DATA_HORA, LOG_CONVERSA)
     VALUES ('47495856882', '48121548421', '2018-02-11 09:12:43-03', './chat_logs/2018-02-11_09:12:43-03.log');
 -- Giovani e Kazalbe  
@@ -86,15 +88,22 @@ INSERT INTO CONVERSA (CLIENTE, NEGOCIANTE, DATA_HORA, LOG_CONVERSA)
     VALUES ('46975897859' , '44455566677', '2019-12-12 11:11:11-03', './chat_logs/2019-12-12_11:11:11-03.log');
 
 -- IMOVEL
--- Marcelo Agiota - Proprietario
+-- Marcelo - Proprietario
 INSERT INTO IMOVEL (RUA, NUMERO, COMPLEMENTO, BAIRRO, CEP, CIDADE, LOCADOR, EH_PROPRIETARIO, PRECO, AREA, NRO_QUARTOS, NRO_SALAS, NRO_BANHEIROS, DESCRICAO_GERAL) 
     VALUES ('Rua Jacinto Favoreto', 573, 'apto13', 'Lutfalla', '13560515', 'São Carlos', '47495856882', TRUE, 900.0, 100.0, 2, 1, 1, 'Apartamento primeiro andar, ótimo estado e com mobília. Vem com vaga no térreo');
--- Carlos Bife - Proprietário
+-- Carlos Bife - Proprietario
 INSERT INTO IMOVEL (RUA, NUMERO, COMPLEMENTO, BAIRRO, CEP, CIDADE, LOCADOR, EH_PROPRIETARIO, PRECO, AREA, NRO_QUARTOS, NRO_SALAS, NRO_BANHEIROS, DESCRICAO_GERAL)
     VALUES ('Rua Jacinto Favoreto', 512, 'Apto 51', 'Jardim Lutfalla', '13560515','Sao Carlos','48121548421',TRUE, 1400.0, 90.0, 2, 1, 1, 'Apartamento novo, ultimo andar, de frente com o Campus I da USP de São Carlos, vem com vaga de garagem e sem mobília');
--- 
+-- Craque Neto - Não proprietario
 INSERT INTO IMOVEL (RUA, NUMERO, COMPLEMENTO, BAIRRO, CEP, CIDADE, LOCADOR, EH_PROPRIETARIO, PRECO, AREA, NRO_QUARTOS, NRO_SALAS, NRO_BANHEIROS, DESCRICAO_GERAL)
-    VALUES ('Rua Jacinto Favoreto', 512, 'Apto 51', 'Jardim Lutfalla', '13560515','Sao Carlos','48487098819',FALSE, 1400.0, 90.0, 2, 1, 1, 'Apartamento novo, com duas vagas. Dois quartos muito bons para estudantes devido a otima localização.');
+    VALUES ('Rua Jacinto Favoreto', 512, 'Apto 51', 'Jardim Lutfalla', '13560515','Sao Carlos','11111111111',FALSE, 700.0, 90.0, 2, 1, 1, 'Apartamento novo, com duas vagas. Dois quartos muito bons para estudantes devido a otima localização.');
+-- Kazalbe - Proprietario    
+INSERT INTO IMOVEL (RUA, NUMERO, COMPLEMENTO, BAIRRO, CEP, CIDADE, LOCADOR, EH_PROPRIETARIO, PRECO, AREA, NRO_QUARTOS, NRO_SALAS, NRO_BANHEIROS, DESCRICAO_GERAL)
+    VALUES ('Rua Episcopal', 2220, NULL, 'Centro', '11111111', 'Sao Carlos','44455566677', TRUE, 2399.99, 108.9, 4, 2, 2, 'Casa historica reformada para abrigar uma nova familia! Otimo estado e mobilias recem compradas! Venha morar na historia!');
+-- Joao - Não proprietario
+INSERT INTO IMOVEL (RUA, NUMERO, COMPLEMENTO, BAIRRO, CEP, CIDADE, LOCADOR, EH_PROPRIETARIO, PRECO, AREA, NRO_QUARTOS, NRO_SALAS, NRO_BANHEIROS, DESCRICAO_GERAL)
+    VALUES ('Rua Episcopal', 2220, NULL, 'Centro', '11111111', 'Sao Carlos','48487098819', FALSE, 600.0, 108.9, 4, 2, 2, 'Nova Republica Saint Charles: Casa com bastante espaço, 4 quartos com um cachorro, o flasco #MoreEmRep');
+
 
 -- FOTO_IMOVEL
 
@@ -126,9 +135,15 @@ INSERT INTO FOTO_ALUGUEL (ID_IMOVEL, PATH_FOTO)
     VALUES (3,'.\photos/imovel/3.2');
 
 -- ALUGUEL 
-
+-- Giovani alugando APARTAMENTO de Marcelo (proprietario)
 INSERT INTO ALUGUEL (CLIENTE, ID_IMOVEL, DATA_INICIO, DATA_TERMINO, DURACAO, DESCRICAO, CUSTO_RESCISAO_INTEGRAL) 
-    VALUES ()
+    VALUES ('46975897859', 1, '2018-10-01 07:12:10-03', '2019-10-01 07:12:10-03','P1Y', 'Aluguel - Minimo 1 ano de aluguel - 900 reais/mes incluso gás',10800.0)
+-- Craque Neto alugando APARTAMENTO de Carlos Bife (proprietario)
+INSERT INTO ALUGUEL (CLIENTE, ID_IMOVEL, DATA_INICIO, DATA_TERMINO, DURACAO, DESCRICAO, CUSTO_RESCISAO_INTEGRAL) 
+    VALUES ('11111111111', 2, '2018-02-02 04:05:05-03', '2019-12-02 05:05:05-03','P1Y10MT1H', 'Aluguel - Minimo 1 ano de aluguel - 900 reais/mes incluso gás',10800.0)
+-- Joao alugando APARTAMENTO de Kazalbe (proprietario)
+INSERT INTO ALUGUEL (CLIENTE, ID_IMOVEL, DATA_INICIO, DATA_TERMINO, DURACAO, DESCRICAO, CUSTO_RESCISAO_INTEGRAL) 
+    VALUES ('48487098819', 2, '2018-02-02 04:05:05-03', '2019-12-02 05:05:05-03','P1Y10MT1H', 'Aluguel - Minimo 1 ano de aluguel - 900 reais/mes incluso gás',10800.0)
 
 -- VAGA
 
@@ -157,7 +172,12 @@ INSERT INTO SERVICO (CLIENTE, PRESTADOR_SERVICO, DATA_HORA, TEMPO_ESTIMADO)
 
 -- VEICULO
 
-INSERT INTO VEICULO (PLACA, PRESTADOR, MARCA, MODELO, ANO, NRO_EIXOS, CAPACIDADE, FOTO1, FOTO2, FOTO3, FOTO4)
+INSERT INTO VEICULO (PLACA, PRESTADOR, MARCA, MODELO, ANO, FOTO1, FOTO2, FOTO3, FOTO4)
+    VALUES()
+
+-- VEICULO_INFO
+
+INSERT INTO VEICULO (MARCA, MODELO, NRO_EIXOS, CAPACIDADE)
     VALUES()
 
 -- PRODUTO
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# BD - parte3
+# Banco de Dados, parte3 - Projeto Final da disciplina
+
 Trabalho para a disciplina SCC0240 - Bases de Dados, aonde iremos construir uma base de dados para nosso projeto de economia colaborativa baseada na temática de "mudança de casa".
+
+Para conseguir construir a base de dados com sucesso, siga as seguintes instruções:
+
+1. Instale o Docker na sua máquina [https://docs.docker.com/get-docker/]. Este programa será necessário para rodar os containers que contém a base de dados PostgreSQL e o pgAdmin (interface gráfica para interagir com a base).
+
+2. Rode o comando `docker-compose up -d`. Caso você não tenha as imagens do Postgres e do pgAdmin, essa parte levará um tempo maior pois o Docker irá baixar essas imagens para você.
+
+3. Espere ele dar "ok" para todas as partes que formam a "network". Assim que tudo estiver certo, já pode começar a interagir com o banco!
+
+_OBS: configuramos o Docker para já executar os scripts "schema" e "data-setup" antes de liberar o acesso ao banco, então você já poderá se conectar com um banco populado sem precisar tocar nos arquivos sql ou mexer no psql!_
