9a0a02bcd8e63ce4df98fc41c73e36907c880728
Vitor Santana Cordeiro <vitorsanusp@gmail.com>
2020-07-18 22:17:36 -0300

    almost finished
    Co-authored-by: JVSRamos <joaoramosj@usp.br>
    Co-authored-by: MarceloMoraesJr <marcelo.junior@usp.br>
    Co-authored-by: giovanidl <giovanilucafo@usp.br>

--- dbadmin.py
+++ dbadmin.py
@@ -13,34 +13,41 @@ def insert(veic):
                                port="15432",
                                database="bd_mudanca")
 
-        cursor = con.cursor()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Database Error: ", error)
+        return "Erro de conexão"
 
-        search = "SELECT V.NRO_EIXOS, V.CAPACIDADE FROM VEICULO_INFO V WHERE UPPER(V.MODELO) = UPPER(%s) AND UPPER(V.ANO) = UPPER(%s);"
-        data_to_search = [veic["modelo"], veic["ano"]]
+    cursor = con.cursor()
 
+    # First search if the model and the year of the veicule already exists
+    search = "SELECT V.NRO_EIXOS, V.CAPACIDADE FROM VEICULO_INFO V WHERE UPPER(V.MODELO) = UPPER(%s) AND UPPER(V.ANO) = UPPER(%s);"
+    data_to_search = [veic["modelo"], veic["ano"]]
+
+    try:
         cursor.execute(search, data_to_search)
         v_info = cur.fetchone()
 
-        if(v_info == None):
-            insert_query = "INSERT INTO VEICULO_INFO (MODELO, ANO, NRO_EIXOS, CAPACIDADE) VALUES (%s,%s,%s,%s)"
-            data_to_insert = [veic["modelo"], veic["ano"], 2, 1000]
-            cursor.execute(insert_query, data_to_insert)
-            con.commit()
-
-        # Inserting the veiculo
-        insert_query = " INSERT INTO VEICULO (PLACA, PRESTADOR, MARCA, MODELO, ANO, FOTO1, FOTO2, FOTO3, FOTO4) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);"
-        data_to_insert = []
-        data_to_insert = [value for key, value in veic.items()]
-        # Insert the last 3 photos
-        [data_to_insert.append(None) for i in range(3)]
-
-        cursor.execute(insert_query, data_to_insert)
-
-        con.commit()
-        cursor.close()
-
     except (Exception, psycopg2.DatabaseError) as error:
         print("Database Error: ", error)
+        return "Erro na consulta das infromações do modelo e ano do veículo"
+
+    if(v_info == None):
+        insert_query = "INSERT INTO VEICULO_INFO (MODELO, ANO, NRO_EIXOS, CAPACIDADE) VALUES (%s,%s,%s,%s)"
+        data_to_insert = [veic["modelo"], veic["ano"], 2, 1000]
+        cursor.execute(insert_query, data_to_insert)
+        con.commit()
+
+    # Inserting the veiculo
+    insert_query = " INSERT INTO VEICULO (PLACA, PRESTADOR, MARCA, MODELO, ANO, FOTO1, FOTO2, FOTO3, FOTO4) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);"
+    data_to_insert = []
+    data_to_insert = [value for key, value in veic.items()]
+    # Insert the last 3 photos
+    [data_to_insert.append(None) for i in range(3)]
+
+    cursor.execute(insert_query, data_to_insert)
+
+    con.commit()
+    cursor.close()
 
     finally:
         # closing database connection.
@@ -53,6 +60,7 @@ def insert(veic):
 def search(capacidade):
 
     con = None
+    transp = []
     try:
         con = psycopg2.connect(user="postgres",
                                password="ribamar",
@@ -62,8 +70,8 @@ def search(capacidade):
 
         cursor = con.cursor()
 
-        search = "SELECT V.NRO_EIXOS, V.CAPACIDADE FROM VEICULO_INFO V WHERE UPPER(V.MODELO) = UPPER(%s) AND UPPER(V.ANO) = UPPER(%s);"
-        data_to_search = [veic["modelo"], veic["ano"]]
+        search = "SELECT MODELO, ANO, CAPACIDADE, N.NOME, N.EMAIL, N.TELEFONE FROM NEGOCIANTE N JOIN VEICULO V ON N.CPF = V.PRESTADOR NATURAL JOIN VEICULO_INFO WHERE CAPACIDADE > %s;"
+        data_to_search = [capacidade]
 
         cursor.execute(search, data_to_search)
         transp = cursor.fetchall()
--- interface.py
+++ interface.py
@@ -139,17 +139,10 @@ class Cadastro(tk.Frame):
                                 bg=colors['sec'],
                                 fg=colors['black'],
                                 font=controller.sm_font,
-                                command=lambda: self.send_data()
+                                command=lambda: self.send_data(
+                                    placa_e, marca_e, modelo_e, ano_e)
                                 )
         send_button.pack(side='right', padx=40, ipady=10, ipadx=100)
-
-        def makeentry(parent, caption, width=None, **options):
-            Label(parent, text=caption).pack(side=LEFT)
-            entry = Entry(parent, **options)
-            if width:
-                entry.config(width=width)
-            entry.pack(side=LEFT)
-            return entry
 
         # inputs: placa, marca, modelo e ano
         placa_f = tk.Frame(input_container, bg=colors['main'])
@@ -202,12 +195,25 @@ class Cadastro(tk.Frame):
                                 )
         back_button.pack(side='left', padx=30, ipady=0)
 
-    def send_data(self):
+    def send_data(self, placa, marca, modelo, ano):
         # CPF do Carlos...
-        self.CPF = 48121548421
+        CPF = 48121548421
         # normalmente, o sistema saberia isso por meio de tecnologias como cookies.
         # Por simplicidade, decidimos fazer para um prestador já logado no sistema,
         # além de fazer a Foto1 hardcoded (já que o objetivo não é fazer uma interface complexa).
+        data = {}
+        data['placa'] = placa.get()
+        data['prestador'] = CPF
+        data['marca'] = marca.get()
+        data['modelo'] = modelo.get()
+        data['ano'] = ano.get()
+        data['foto1'] = f"./photos/vehicles/{placa.get()}-1.jpeg"
+        # errors = insert(data)
+        errors = ['CEIS', 'TAO', 'DE', 'SACANAGEM']
+        if (errors):
+            tk.messagebox.showerror(
+                "Um ou mais erros foram identificados", errors)
+        print(data, errors)
 
 
 class Busca(tk.Frame):
@@ -223,35 +229,64 @@ class Busca(tk.Frame):
                         background=colors['main'],
                         foreground=colors['sec']
                         )
-        text.pack(side="top", fill="x", pady=60)
+        text.pack(side="top", fill="x", pady=30)
 
-        # query_container = tk.Frame(self, background=colors['black'])
-        # query_container.pack(fill="x", expand=True,
-        #                      pady=10, padx=40)
+        welcome_text = tk.Label(self, text="Digite a carga minima necessaria",
+                                font=controller.m_font,
+                                background=colors['main'],
+                                foreground=colors['sec']
+                                )
+        welcome_text.pack(side="top", fill="x", pady=0)
 
-        # xpad = 100
-        # ypad = 50
-        # iypad = 10
-        # back_button.pack(side='top', fill='x', padx=xpad, ipady=iypad)
+        query_container = tk.Frame(self, background=colors['main'])
+        query_container.pack(fill="x", expand=True,
+                             pady=0, padx=40)
 
-        # # input de carga minima:
-        # carga_f = tk.Frame(query_container)
-        # carga_l = tk.Label(placa_f, text="Carga minima")
-        # carga_e = tk
+        input_container = tk.Frame(query_container, background=colors['main'])
+        input_container.pack(side='left', ipadx=60)
 
-        # carga_f.pack()
+        send_button = tk.Button(query_container, text="ENVIAR",
+                                bg=colors['sec'],
+                                fg=colors['black'],
+                                font=controller.sm_font,
+                                command=lambda: self.send_data()
+                                )
+        send_button.pack(side='right', padx=0, ipady=0)
 
-        button_container = tk.Frame(self, background=colors['black'])
-        button_container.pack(fill="x", expand=True, pady=10)
+        button_container = tk.Frame(self, background=colors['main'])
+        button_container.pack(side='bottom', fill="x", pady=30)
+
+        carga_f = tk.Frame(input_container, bg=colors['main'])
+        carga_l = tk.Label(
+            carga_f, bg=colors['main'], fg=colors['sec'], text="Carga", font=controller.sm_font)
+        carga_e = tk.Entry(carga_f, bg=colors['white'])
 
         back_button = tk.Button(button_container, text="VOLTAR",
                                 bg=colors['sec'],
                                 fg=colors['black'],
-                                font=controller.m_font,
+                                font=controller.sm_font,
                                 command=lambda: controller.show_frame(
                                     "Inicio")
                                 )
         back_button.pack(side='left', padx=30, ipady=0)
+
+        carga_f.pack(side='left')
+        carga_l.pack(side='left')
+        carga_e.pack(side='left')
+
+        scrollbar = tk.Scrollbar(self)
+        scrollbar.pack(side='right', fill='y')
+        textbox = tk.Text(self)
+
+        textbox.pack()
+
+        textbox.insert('e', "XUPA JAO SEU FILHO DA PUTA\n")
+        # attach textbox to scrollbar
+        textbox.config(yscrollcommand=scrollbar.set)
+        scrollbar.config(command=textbox.yview)
+
+    def send_data(self):
+        print("Pesquisei")
 
 
 if __name__ == "__main__":
